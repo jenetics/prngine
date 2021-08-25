70e863961d708bbf0bb3bf1609e0ee28c7276102
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2021-08-25 22:07:37 +0200

    #31: Add some experimental tests.

--- prngine/src/test/java/io/jenetics/prngine/RandomGeneratorServiceTest.java
+++ prngine/src/test/java/io/jenetics/prngine/RandomGeneratorServiceTest.java
@@ -1,4 +1,25 @@
 package io.jenetics.prngine;
 
+import java.util.random.RandomGenerator;
+import java.util.random.RandomGeneratorFactory;
+
 public class RandomGeneratorServiceTest {
+
+	public static void main(final String[] args) {
+		RandomGeneratorFactory.all().forEach(f -> {
+			final var r = f.create();
+			System.out.println(r);
+		});
+
+		final var random = RandomGenerator.of("KISS64Random");
+		System.out.println();
+		System.out.println(random);
+
+		final var rf = RandomGeneratorFactory.of("KISS64Random");
+		System.out.println();
+		System.out.println(rf);
+		System.out.println(rf.create(12));
+		System.out.println(rf.create(new byte[]{1, 2}));
+	}
+
 }
