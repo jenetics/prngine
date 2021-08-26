1f74f4e819956a8f4b9e73efb7008223dd65495e
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2021-08-26 21:30:32 +0200

    #31: Add dieharder test scripts.

---
+++ dieharder_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+VERSION='2.0.0-SNAPSHOT'
+
+java -cp prngine/build/libs/prngine-${VERSION}.jar io.jenetics.prngine.internal.DieHarder KISS32Random -a
+java -cp prngine/build/libs/prngine-${VERSION}.jar io.jenetics.prngine.internal.DieHarder KISS64Random -a
+java -cp prngine/build/libs/prngine-${VERSION}.jar io.jenetics.prngine.internal.DieHarder LCG64ShiftRandom -a
+java -cp prngine/build/libs/prngine-${VERSION}.jar io.jenetics.prngine.internal.DieHarder MT19937_32Random -a
+java -cp prngine/build/libs/prngine-${VERSION}.jar io.jenetics.prngine.internal.DieHarder MT19937_64Random -a
+java -cp prngine/build/libs/prngine-${VERSION}.jar io.jenetics.prngine.internal.DieHarder XOR32ShiftRandom -a
+java -cp prngine/build/libs/prngine-${VERSION}.jar io.jenetics.prngine.internal.DieHarder XOR64ShiftRandom -a
