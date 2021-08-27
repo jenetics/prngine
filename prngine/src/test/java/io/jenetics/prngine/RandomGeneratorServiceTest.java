d70b4bfbcae1414a1e4081fdc1b086c27aa70adc
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2021-08-27 21:10:21 +0200

    #31: Add service registering test.

--- prngine/src/test/java/io/jenetics/prngine/RandomGeneratorServiceTest.java
+++ prngine/src/test/java/io/jenetics/prngine/RandomGeneratorServiceTest.java
@@ -3,23 +3,43 @@ package io.jenetics.prngine;
 import java.util.random.RandomGenerator;
 import java.util.random.RandomGeneratorFactory;
 
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 public class RandomGeneratorServiceTest {
 
-	public static void main(final String[] args) {
-		RandomGeneratorFactory.all().forEach(f -> {
-			final var r = f.create();
-			System.out.println(r);
-		});
+	@Test(dataProvider = "names")
+	public void creation(final String name) {
+		final var random = RandomGenerator.of(name);
+		Assert.assertNotNull(random);
 
-		final var random = RandomGenerator.of("KISS64Random");
-		System.out.println();
-		System.out.println(random);
+		final var factory = RandomGeneratorFactory.of(name);
+		Assert.assertNotNull(factory);
 
-		final var rf = RandomGeneratorFactory.of("KISS64Random");
-		System.out.println();
-		//System.out.println(rf + ":" + rf.stateBits());
-		System.out.println(rf.create(12));
-		System.out.println(rf.create(new byte[]{1, 2}));
+		final var random2 = factory.create();
+		Assert.assertEquals(random2.getClass(), random.getClass());
+
+		final var random3 = factory.create(System.currentTimeMillis());
+		Assert.assertEquals(random3.getClass(), random.getClass());
+
+		final var seed = new byte[10_000];
+		random.nextBytes(seed);
+		final var random4 = factory.create(seed);
+		Assert.assertEquals(random4.getClass(), random.getClass());
+	}
+
+	@DataProvider
+	public Object[][] names() {
+		return new Object[][] {
+			{"KISS32Random"},
+			{"KISS64Random"},
+			{"LCG64ShiftRandom"},
+			{"MT19937_32Random"},
+			{"MT19937_64Random"},
+			{"XOR32ShiftRandom"},
+			{"XOR64ShiftRandom"}
+		};
 	}
 
 }
