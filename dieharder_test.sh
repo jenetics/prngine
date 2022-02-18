a1842ff6dacdb2ead5cbb58ada14f8c526611be2
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2022-02-18 08:23:39 +0100

    Update test scripts.

--- dieharder_test.sh
+++ dieharder_test.sh
@@ -2,10 +2,11 @@
 
 VERSION='2.0.0-SNAPSHOT'
 
-java -cp prngine/build/libs/prngine-2.0.0-SNAPSHOT.jar io.jenetics.prngine.internal.DieHarder KISS32Random -a
-java -cp prngine/build/libs/prngine-2.0.0-SNAPSHOT.jar io.jenetics.prngine.internal.DieHarder KISS64Random -a
-java -cp prngine/build/libs/prngine-2.0.0-SNAPSHOT.jar io.jenetics.prngine.internal.DieHarder LCG64ShiftRandom -a
-java -cp prngine/build/libs/prngine-2.0.0-SNAPSHOT.jar io.jenetics.prngine.internal.DieHarder MT19937_32Random -a
-java -cp prngine/build/libs/prngine-2.0.0-SNAPSHOT.jar io.jenetics.prngine.internal.DieHarder MT19937_64Random -a
-java -cp prngine/build/libs/prngine-2.0.0-SNAPSHOT.jar io.jenetics.prngine.internal.DieHarder XOR32ShiftRandom -a
-java -cp prngine/build/libs/prngine-2.0.0-SNAPSHOT.jar io.jenetics.prngine.internal.DieHarder XOR64ShiftRandom -a
+java -cp prngine/build/libs/prngine-${VERSION}.jar io.jenetics.prngine.internal.DieHarder KISS32Random -a
+java -cp prngine/build/libs/prngine-${VERSION}.jar io.jenetics.prngine.internal.DieHarder KISS64Random -a
+java -cp prngine/build/libs/prngine-${VERSION}.jar io.jenetics.prngine.internal.DieHarder LCG64ShiftRandom -a
+java -cp prngine/build/libs/prngine-${VERSION}T.jar io.jenetics.prngine.internal.DieHarder MT19937_32Random -a
+java -cp prngine/build/libs/prngine-${VERSION}.jar io.jenetics.prngine.internal.DieHarder MT19937_64Random -a
+java -cp prngine/build/libs/prngine-${VERSION}.jar io.jenetics.prngine.internal.DieHarder XOR32ShiftRandom -a
+java -cp prngine/build/libs/prngine-${VERSION}.jar io.jenetics.prngine.internal.DieHarder XOR64ShiftRandom -a
+java -cp prngine/build/libs/prngine-${VERSION}.jar io.jenetics.prngine.internal.DieHarder L64X256MixRandom -a
